64e6703371417c302e27aaf99b6b3d9c3b7e01e1
lilyLILYLL <lydatily@gmail.com>
2024-01-02 11:52:19 +1100

    VCB

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@ import express from "express";
 import { contactRoute } from "./contactRoute";
 
 export const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.status(200).json("Welcome, your app is working well");
 });
 
 app.use("/", contactRoute);
-app.listen(port, () => {
-    console.log("Listening to", port);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log("Listening to", PORT);
 });
