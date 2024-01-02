bc309115701ed2fe2631fb08c83e47b1bf0f9d8a
lilyLILYLL <lydatily@gmail.com>
2024-01-02 11:56:08 +1100

    gitingore

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@ import express from "express";
 import { contactRoute } from "./contactRoute";
 
 export const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3005;
 
 app.get("/", (req, res) => {
     res.status(200).json("Welcome, your app is working well");
 });
 
 app.use("/", contactRoute);
-app.listen(PORT, "0.0.0.0", () => {
-    console.log("Listening to", PORT);
+app.listen(port, () => {
+    console.log("Listening to", port);
 });
