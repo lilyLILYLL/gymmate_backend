7a8496f858bfeab765709fd122621231e81d5da2
lilyLILYLL <lydatily@gmail.com>
2024-01-02 18:59:17 +1100

    config

--- config.ts
+++ config.ts
@@ -3,5 +3,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const port = process.env.PORT || 5000;
-export const ACCESS_TOKEN_KEY = process.env.ACCESS_TOKEN_KEY || "";
-export const CONNECTION_STRING = process.env.CONNECTION_STRING || "";
+export const ACCESS_TOKEN_KEY = process.env.ACCESS_TOKEN_KEY as string;
+export const CONNECTION_STRING = process.env.CONNECTION_STRING as string;
--- src/database/dbConnection.ts
+++ src/database/dbConnection.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 import { CONNECTION_STRING } from "../../config";
 export const connectDb = async () => {
+    console.log(CONNECTION_STRING);
     try {
-        const connection = await mongoose.connect(CONNECTION_STRING );
+        const connection = await mongoose.connect(CONNECTION_STRING);
     } catch (err) {
         console.log(err);
         process.exit(1);
