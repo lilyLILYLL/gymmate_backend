1f257f10d250a31e556a5ccef29af1993008c876
lilyLILYLL <lydatily@gmail.com>
2024-01-02 11:47:00 +1100

    add route to the app

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import { contactRoute } from "./contactRoute";
 
-const app = express();
+export const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.use(contactRoute);
+app.use("/", contactRoute);
 app.listen(port, () => {
     console.log("Listening to", port);
 });
--- src/contactRoute.ts
+++ src/contactRoute.ts
@@ -3,5 +3,5 @@ import express from "express";
 export const contactRoute = express.Router();
 
 contactRoute.get("/lilly", (req, res) => {
-    res.send("Hello Lilly");
+    res.send("Hello fgdjgj");
 });
